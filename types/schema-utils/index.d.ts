d8f6d5d82bc0a7016d59f15793ab0cb7424728ef
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-12-09 16:41:27 +0300

    update dependencies

--- types/schema-utils/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'schema-utils' {
-	import {JSONSchema4, JSONSchema6, JSONSchema7} from 'json-schema';
-
-	function validateOptions(schema: JSONSchema4 | JSONSchema6 | JSONSchema7, options: object, configuration?: object): void;
-
-	export = validateOptions;
-}
